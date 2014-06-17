aa6f72330fbee08231f717c379b4fc8f1ac4a122
Tyrell Keene <tyrell.wkeene@gmail.com>
2014-06-17 02:42:28 -0600

    [CLEANUP]: Remove redundant commit patch generation from do-tag.sh

--- do-tag.sh
+++ do-tag.sh
@@ -54,17 +54,3 @@ if [ $patch_tags == 1 ]; then
 		git diff -p master $1 > $patch_dir/tags/master-$1.patch
 	fi
 fi
-
-if [ $patch_commits == 1 ]; then
-	printf "Generating by-commit patch\n"
-	if [ $review_patches == 1 ]; then
-		git format-patch -n --stdout -s --ignore-all-space HEAD^ | less
-		if yn "Generate this patch?"; then
-			git format-patch -n --stdout -s --ignore-all-space HEAD^ >> $patch_dir/commits/`git rev-list \
-				--max-count=1 --abbrev-commit HEAD^`.patch
-		fi
-	else
-		git format-patch -n --stdout -s --ignore-all-space HEAD^ >> $patch_dir/commits/`git rev-list \
-			--max-count=1 --abbrev-commit HEAD^`.patch
-	fi
-fi
