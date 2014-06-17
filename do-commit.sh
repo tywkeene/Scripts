3c31be09e26080c8895fccaa5117c9921c95552e
Tyrell Keene <tyrell.wkeene@gmail.com>
2014-06-17 00:52:49 -0600

    [FEATURE]: Added commit type to do-commit.sh

--- do-commit.sh
+++ do-commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-	printf "Usage $0 <commit message>\n"
+if [ -z "$1" ] || [ -z "$2" ]; then
+	printf "Usage $0 <commit type [BUGFIX/FEATURE/CLEANUP]> <commit message >\n"
 	exit 1
 fi
 
-git commit -am $1
+git commit -m "[$1]: $2"
 
 git format-patch -n --stdout -s --ignore-all-space HEAD^ >> .patches/commits/`git rev-list \
 		--max-count=1 --abbrev-commit HEAD^`.patch
