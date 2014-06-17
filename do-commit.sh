5ce4409d38123a33f918c2aad52b30979ea7222f
Tyrell Keene <tyrell.wkeene@gmail.com>
2014-06-17 02:14:36 -0600

    [FEATURE]: Added setup.sh (generates .patch_setting and creates .patches/)

--- do-commit.sh
+++ do-commit.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+yn () {
+	while true; do
+		read -p "$1 " yn
+		case $yn in
+			[Yy]* ) return 0;;
+		[Nn]* ) return 1;;
+	* ) echo "Please answer yes or no.";;
+esac
+    done
+}
+
+if [ ! -f .patch_settings ]; then
+	printf "Couldn't file settings files, exiting\n"
+	exit -1
+fi
+
+source .patch_settings
+
 if [ -z "$1" ] || [ -z "$2" ]; then
 	printf "Usage $0 <commit type [BUGFIX/FEATURE/CLEANUP]> <commit message >\n"
 	exit 1
@@ -7,5 +25,15 @@ fi
 
 git commit -m "[$1]: $2"
 
-git format-patch -n --stdout -s --ignore-all-space HEAD^ >> .patches/commits/`git rev-list \
-		--max-count=1 --abbrev-commit HEAD^`.patch
+if [ $patch_commits == 1 ]; then
+	if [ $review_patches == 1 ]; then
+		git format-patch -n --stdout -s --ignore-all-space HEAD^ | less
+		if yn "Generate this patch?"; then
+			git format-patch -n --stdout -s --ignore-all-space HEAD^ >> $patch_dir/commits/`git rev-list \
+				--max-count=1 --abbrev-commit HEAD^`.patch
+		fi
+	else
+		git format-patch -n --stdout -s --ignore-all-space HEAD^ >> $patch_dir/commits/`git rev-list \
+			--max-count=1 --abbrev-commit HEAD^`.patch
+	fi
+fi
